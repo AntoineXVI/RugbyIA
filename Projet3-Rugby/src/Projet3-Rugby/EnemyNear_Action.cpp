bd8225ee2d555dafb0be6a45c98b5bb01dd03890
AntoineXVI <antoinexvipichard@gmail.com>
2024-10-25 18:58:53 +0200

    gamemanager clean

--- Projet3-Rugby/src/Projet3-Rugby/EnemyNear_Action.cpp
+++ Projet3-Rugby/src/Projet3-Rugby/EnemyNear_Action.cpp
@@ -15,7 +15,8 @@ void EnemyNear_Action::Start(Player* player)
 
 void EnemyNear_Action::Update(Player* player)
 {
-	player->goToPosition(sf::Vector2f(player->getPosition().x+10, player->getPosition().y+10));
+	//player->goToPosition(sf::Vector2f(player->getPosition().x+10, player->getPosition().y+10));
+	//faire une passe
 }
 
 void EnemyNear_Action::End(Player* player)
